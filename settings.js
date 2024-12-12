b811230193d10ab2d26497c0200c9244d0675e21
SLnadu <165290825+SLnadu@users.noreply.github.com>
2024-12-12 20:20:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=jiYnxLRI#4ttfkaQDe_B4YoV9VI_95iX9VhLSD_t02MMjZEBTQk0' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Oqx3mSBZ#euaNL0TtfFcpx7fFqwfCJbtyYxSCpV7b-BnacVGMBw0' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now !!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
